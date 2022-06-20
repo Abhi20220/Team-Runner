62fd53c4c931cd23d8031e34477eec52e89b86a7
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-06-20 18:27:10 +0930

    removed custom port

--- server.js
+++ server.js
@@ -39,7 +39,3 @@ app.use(routes)
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'))
 })
-
-app.listen(PORT , () =>
-console.log(`App listening to http://localhost:${PORT}`) 
-);
