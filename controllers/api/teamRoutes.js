be20279b7d3b60c93047aeb6c55b0a44eedbd10d
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-06-20 13:22:10 +0930

    team routes and local port

--- controllers/api/teamRoutes.js
+++ controllers/api/teamRoutes.js
@@ -1,5 +1,36 @@
 const router = require('express').Router()
-const { Team } = require('../../models')
+const { Team, Run } = require('../../models')
 const withAuth = require('../../utils/auth')
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newTeam = await Team.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        })
+        res.status(200).json(newTeam)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const teamData = await Run.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        })
+
+        if (!teamData) {
+            res.status(404).json({message: 'No Team!'})
+            return
+
+        }
+        res.status(200).json(teamData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 module.exports = router
--- server.js
+++ server.js
@@ -39,3 +39,7 @@ app.use(routes)
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'))
 })
+
+app.listen(PORT , () =>
+console.log(`App listening to http://localhost:${PORT}`)
+);
