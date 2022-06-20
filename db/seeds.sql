1575ab576445452326901b2efee7cb421afcac09
Marcus Vinicius <marcuslops.au@gmail.comgit congif user.emailgit config --global user.name Marcus>
2022-06-20 21:12:18 +0930

    update seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO running_db (email, firstName, lastName, password)
+
+VALUES(001, "@gmail.com", "Fname1", "Sname1", ? ),
+(002, "@gmail.com", "Fname2", "Sname2", ? ),
+(003, "@gmail.com", "Fname3", "Sname3", ? ),
+(004, "@gmail.com", "Fname4", "Sname4", ? ),
+(005, "@gmail.com", "Fname5", "Sname5", ? ),
+(006, "@gmail.com", "Fname6", "Sname6", ? )
