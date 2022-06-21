bd8fb1902fc40d5065bedcbf7a9c0c5e7ee01afa
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-06-21 15:10:56 +0930

    homeroutes files changed run data linked to user data

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,7 +3,7 @@ const { User } = require('../../models')
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body)
+    const userData = await User.create(req.body,)
 
     req.session.save(() => {
       req.session.user_id = userData.id
@@ -17,9 +17,13 @@ router.post('/', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
+  
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } })
-
+    const userData = await User.findOne({ 
+      where: { email: req.body.email},
+ 
+    })
+    
     if (!userData) {
       res
         .status(400)
@@ -35,12 +39,17 @@ router.post('/login', async (req, res) => {
         .json({ message: 'Incorrect email or password, please try again' })
       return
     }
-
+    
+    
     req.session.save(() => {
       req.session.user_id = userData.id
       req.session.logged_in = true
+      userData.password = ""
+      
 
-      res.json({ user: userData, message: 'You are now logged in!' })
+      res.status(200).json({ user: userData, message: 'You are now logged in!' }
+      
+)
     })
   } catch (err) {
     res.status(400).json(err)
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
-const { Post, User } = require('../models')
+const { Run, User } = require('../models')
 const withAuth = require('../utils/auth')
 
 router.get('/', async (req, res) => {
   try {
-    // Get all posts and JOIN with user data
-    const postData = await Post.findAll({
+    // Get all run data and JOIN with user data
+    const runData = await Run.findAll({
       include: [
         {
           model: User,
@@ -15,11 +15,11 @@ router.get('/', async (req, res) => {
     })
 
     // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }))
+    const runs = runData.map((Run) => Run.get({ plain: true }))
 
     // Pass serialized data and session flag into template
     res.render('homepage', {
-      posts,
+        runs,
       logged_in: req.session.logged_in,
     })
   } catch (err) {
@@ -27,9 +27,9 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.get('/post/:id', async (req, res) => {
+router.get('/run/:id', async (req, res) => {
   try {
-    const postData = await Post.findByPk(req.params.id, {
+    const runData = await Run.findByPk(req.params.id, {
       include: [
         {
           model: User,
@@ -38,10 +38,10 @@ router.get('/post/:id', async (req, res) => {
       ],
     })
 
-    const post = postData.get({ plain: true })
+    const Run = runData.get({ plain: true })
 
-    res.render('post', {
-      ...post,
+    res.render('Run', {
+      ...Run,
       logged_in: req.session.logged_in,
     })
   } catch (err) {
@@ -55,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
+      include: [{ model: Run }],
     })
 
     const user = userData.get({ plain: true })
