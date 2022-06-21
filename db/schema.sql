7230cbde0ac0939540483db6d6d0d006d3689dc2
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-06-21 20:36:22 +0930

    db schema seeds update

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,26 @@ CREATE DATABASE running_db;
 
 USE running_db;
 
-CREATE TABLE run_statistics (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    times_ran INT NOT NULL,
-    run_distance INT
-
-);
 
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
-    runner_name VARCHAR (30),
     is_active BOOLEAN,
-    age INT,
-    FOREIGN KEY(runner_name)
-    REFERENCES run_statistics (id)
+    age INT
+  
+
+);
+
+CREATE TABLE run_statistics (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    times_ran INT NOT NULL,
+    run_distance INT,
+    user_id INT,
+    FOREIGN KEY(user_id)
+    REFERENCES user (id)
     ON DELETE SET NULL
 
 );
 
 
-delete
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,11 @@
-INSERT INTO run_statistics (times_ran, run_distance)
-VALUES (1, '100km'),  
-       (3, '124km'),
-       (5, '69km'),
-       (9, '96km');
+INSERT INTO user (id, first_name, last_name, is_active, age)
+VALUES (1, "Abhi", "Shek", true, 20),  
+       (2, "Usain", "Bolt", true, 47),
+       (3, "George", "Bush", false, 89);
 
-INSERT INTO user (first_name, last_name, runner_name, is_active)
-VALUES ("Abhi", "Shek", "Beast", true),
-       ("Usain", "Bolt", "Lightning", true),
-       ("George", "Bush", "President", false),
-       ("Alice", "Paprika", "Redlips", true);
 
-//delete
+INSERT INTO run_statistics (times_ran, run_distance, user_id)
+VALUES (4, 88, 1),
+(1, 23, 2),
+(1, 26, 3);
+
