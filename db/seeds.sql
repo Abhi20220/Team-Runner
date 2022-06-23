d14677abd437bcac71064fbfd0ff676a7a61852a
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-06-23 19:33:53 +0930

    commented out sql created new seeds and json data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO user (id, first_name, last_name, email, user_password, is_active, age)
-VALUES  (1, "King", "Arthur", "king@camelot.com", "merlin", true, 24)
-(2, "Wizard", "Merlin", "merlin@camelot.com", "dragon", true, 26)
-(3, "Frodo", "Baggins", "shire@hobbit.com", "thering", false, 16)
+-- INSERT INTO user (id, first_name, last_name, email, user_password, is_active, age)
+-- VALUES  (1, "King", "Arthur", "king@camelot.com", "merlin", true, 24)
+-- (2, "Wizard", "Merlin", "merlin@camelot.com", "dragon", true, 26)
+-- (3, "Frodo", "Baggins", "shire@hobbit.com", "thering", false, 16)
 
 
-INSERT INTO run_statistics (times_ran, run_distance, user_id)
-VALUES (4, 88, 1),
-(1, 23, 2),
-(1, 26, 3);
+-- INSERT INTO run_statistics (times_ran, run_distance, user_id)
+-- VALUES (4, 88, 1),
+-- (1, 23, 2),
+-- (1, 26, 3);
 
