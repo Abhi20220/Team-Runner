576e5c5501f26908f6cf8b1f15a5fae9685e0c73
Kym Reilly <kymreilly@hotmail.com>
2022-06-25 13:03:57 +0930

    working on location tracking and calculation distance

--- public/js/location.js
+++ public/js/location.js
@@ -1,20 +1,93 @@
-const HIGH_ACCURACY = true
-const MAX_CACHE_AGE_MILLISECOND = 20000
-const MAX_NEW_POSITION_MILLISECOND = 8000
+// const startTracking = () => {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.watchPosition(function (position) {
+//       console.log(position)
+//     })
+//   } else {
+//     console.log('Geolocation is not supported')
+//   }
+// }
 
-const trackOptions = {
-  enableHighAccuracy: HIGH_ACCURACY,
-  maximumAge: MAX_CACHE_AGE_MILLISECOND,
-  timeout: MAX_NEW_POSITION_MILLISECOND,
+// const stopTracking = () => {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.watchPosition(function (position) {
+//       console.log(position)
+//     })
+//   } else {
+//     console.log('Geolocation is not supported')
+//   }
+// }
+
+let trackBtn = document.getElementById('trackBtn')
+
+trackBtn.addEventListener('click', Start)
+
+function Start() {
+  console.log('Started')
+  trackBtn.removeEventListener('click', Start)
+  trackBtn.addEventListener('click', Stop)
+  trackBtn.value = 'Stop'
 }
 
-const startTracking = () => {
-  if (!navigator.geolocation) {
-    logConsole.textContent = 'Geolocation is not supported by your browser'
-  } else {
-    logConsole.textContent = 'Locating ...'
-    distanceBox.textContent = '0.000'
+function Stop() {
+  console.log('Stopped')
+  trackBtn.removeEventListener('click', Stop)
+  trackBtn.addEventListener('click', Start)
+  trackBtn.value = 'Start'
+}
 
-    return navigator.geolocation.watchPosition(success, error, trackOptions)
+let watchID
+let geoLoc
+
+function showLocation(position) {
+  let latitude = position.coords.latitude
+  let longitude = position.coords.longitude
+  console.log('Latitude : ' + latitude + ' Longitude: ' + longitude)
+}
+
+function errorHandler(err) {
+  if (err.code == 1) {
+    console.log('Error: Access is denied!')
+  } else if (err.code == 2) {
+    console.log('Error: Position is unavailable!')
   }
+}
+
+function getLocationUpdate() {
+  if (navigator.geolocation) {
+    // timeout at 5000 milliseconds (5 seconds)
+    let options = { timeout: 5000 }
+    geoLoc = navigator.geolocation
+    watchID = geoLoc.watchPosition(showLocation, errorHandler, options)
+  } else {
+    console.log('Sorry, browser does not support geolocation!')
+  }
+}
+
+function stopLocationUpdate() {
+  // Cancel the updates when the user clicks a button.
+  navigator.geolocation.clearWatch(watchID)
+}
+
+alert(calcCrow(38.9285, 138.6007, 37.8136, 144.9631).toFixed(1))
+
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcCrow(lat1, lon1, lat2, lon2) {
+  var R = 6371 // km
+  var dLat = toRad(lat2 - lat1)
+  var dLon = toRad(lon2 - lon1)
+  var lat1 = toRad(lat1)
+  var lat2 = toRad(lat2)
+
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  var d = R * c
+  return d
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) {
+  return (Value * Math.PI) / 180
 }
--- utils/helper.js
+++ utils/helper.js
@@ -8,8 +8,8 @@ module.exports = {
     return parseInt(amount).toLocaleString()
   },
   format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
-    }`
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`
   },
 }
